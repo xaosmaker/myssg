babcc24f7227849cabcfec2f0813d77607e52fba
Drosos <drosinakis.drosos1@gmail.com>
2025-02-10 18:58:28 +0200

    Create ssg and start the first class

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
